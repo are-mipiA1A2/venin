949a075233ab241aa8c57c411116ff496f8d4c05
AREJooVinc <36695442+AREJooVinc@users.noreply.github.com>
2018-03-28 12:43:09 +0200

    Add files via upload

--- InterfaceGraphique.py
+++ InterfaceGraphique.py
@@ -4,6 +4,7 @@ from converter import *
 import time
 import sys  
 import os
+from numpy import *
 
 
 def restart_program():
@@ -11,7 +12,7 @@ def restart_program():
     os.execl(python, python,* sys.argv)
 
 window=Tk()
-canvas=Canvas(window,height=500,width=500,bg="white")
+canvas=Canvas(window,height=510,width=510,bg="white")
 canvas.grid(row=0,column=1)
 
 
@@ -21,38 +22,55 @@ def couleur(p):
 
 
 def draw(x):
-    for column in range (501):
-        for ligne in range (501):
-            canvas.create_rectangle(x*column,x+ligne*x,2*x+x*column,x*ligne,fill=couleur(0),outline="white")
+    for column in range (502):
+        for ligne in range (502):
+            canvas.create_rectangle(x*column,x+ligne*x,2*x+x*column,x*ligne,fill="white", outline = "black")
 
 draw(10) 
 
-canvas.create_oval(-55,-55,555, 555,width=110,outline="grey85" )
-canvas.create_oval(0,0,500,500,width=1,outline="red")
+canvas.create_oval(-70,-70,580, 580,width=138,outline="grey85" )
+canvas.create_oval(0,0,510,510,width=1,outline="black")
+
+#variable
+mat = array([[[1, 0.02245242],
+        [0.05411948, 0.10433879]],
+
+       [[0.02142567, 1],
+        [0.06447764, 0.03263258]],
+
+       [[0.02934682, 0.07481326],
+        [1, 0.09996509]],
+
+       [[0.016496  , 0.09931502],
+        [0.06480892, 1]]])
+
 
 def propagation(x,y,p):
+    
     x1, y1, x2, y2 = convertisseur_coord(x,y)
+    canvas.create_rectangle(x1, y1, x2, y2, fill =couleur(p)) 
 
-    canvas.create_rectangle(x1, y1, x2, y2, fill =couleur(p), outline = "white" ) 
+def prop(mat):
+    x , y, z = shape(mat)
+    for i in range (x):
 
-propagation(24,24,1)
-propagation(24,25,0.8)
-propagation(24,26,0.5)
-propagation(25,24,1)
-propagation(25,25,0.8)
-propagation(25,26,0.5)
-propagation(26,24,1)
-propagation(26,25,0.8)
-propagation(26,26,0.5)
-propagation(27,24,1)
-propagation(27,25,0.8)
-propagation(27,26,0.5)
+        for ligne in range (y):
+            for colonne in range (z):
+                propagation(ligne, colonne, mat[i, ligne, colonne])
+    
+prop(mat)
+
+#propagation(25,25,1)
+#propagation(25,50,1)
+#propagation(25,0,1)
+#propagation(0,25,1)
+#propagation(50,25,1)
 
 
 def donnee():
-    print(5)
+    
 
-print(couleur(0.44))
+    print(couleur(0.44))
 
 
 Button(window,text="Morsure",command=donnee) .grid(row=0,column=0)
